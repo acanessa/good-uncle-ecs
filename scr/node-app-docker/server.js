15b38651531d2562e22467fabf1285431d9b27b1
Augusto Canessa <augustocanessa@gmail.com>
2016-09-09 02:47:28 -0400

    correct file server.js for node-app

--- scr/node-app-docker/server.js
+++ scr/node-app-docker/server.js
@@ -1,15 +1,65 @@
-'use strict';
+/*
+Basic nodejs app to test docker containers in Amazon Elastic Container Services.
+The app will diplay information about the container and will log containerId in a MongoDB database.
+*/
 
-const express = require('express');
+var express = require('express');
+var app = express();
+var os = require("os");
+var mongoose     = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/node-app',function() {console.log('MONGO  [Connected]');});
 
-// Constants
-const PORT = 8080;
+var hostname = os.hostname();
+var cpu = os.cpus();
+var mem = os.freemem();
+var inet = os.networkInterfaces();
+var uptime = os.uptime();
+var feedback = '';
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Testing Docker Enabled App on ECS\n');
+var Schema = mongoose.Schema, ObjectId = Schema.ObjectID;
+
+var InfoSchema = new Schema({
+    desc:       { type: String, required: false},
+    containerId:    { type: String, required: false},
+    //cpu:            { type: String, required: false},
+    mem:            { type: String, required: false},
+    //inet:           { type: String, required: false},
+    uptime:         { type: String, required: false}
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+var infoModel = mongoose.model('Info', InfoSchema);
+
+
+
+app.get('/', function (req, res) {
+    
+    var info = new infoModel({
+        'desc':'Docker container running a Nodejs app.',
+        'containerId':hostname,
+        'mem':mem,
+        'uptime':uptime
+    });
+    
+    info.save( function(error, data){
+        if(error){
+            console.log(error);
+        }
+        else{
+             console.log('Mongo write [OK]');
+        }
+    });
+    
+    var info = {
+        'info':'Docker container running a Nodejs app.',
+        'container-id':hostname,
+        'cpu':cpu,
+        'free-memory':mem,
+        'interfaces':inet,
+        'uptime':uptime
+    }    
+    res.setHeader('Content-Type', 'application/json');    
+    res.send(JSON.stringify(info,null,3));    
+});
+
+app.listen(80);
+console.log('Node-App Running!');
